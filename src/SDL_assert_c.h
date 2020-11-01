0bf1580bf3e04b71c8c0f5be83ea6ec14ce7a35f
Boogermann <marcus@raetro.org>
2020-11-01 13:14:08 -0600

    headers update

--- src/SDL_assert_c.h
+++ src/SDL_assert_c.h
@@ -1,24 +1,33 @@
-/*
-  Simple DirectMedia Layer
-  Copyright (C) 1997-2020 Sam Lantinga <slouken@libsdl.org>
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source distribution.
-*/
-
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*******************************************************************************
+ * Library       : SDLite 1.2.x
+ * Purpose       : Low-level access to a framebuffer, audio output and HID.
+ * Module        : Core
+ * Project       : Redux for Embedded System
+ * Description   : Stripped-down and optimized libraries for RISC processors
+ * License       : GNU General Public License v3.0
+ *******************************************************************************
+ *
+ * Rætro and SDLite 1.2.x:
+ * Copyright (c) 2019-2020 Marcus Andrade <marcus@raetro.org>
+ *
+ * Simple DirectMedia Layer and SDL:
+ * Copyright (c) 1997-2020 Sam Lantinga <slouken@libsdl.org>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.
+ * If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
+ *
+ ******************************************************************************/
 #ifndef SDL_assert_c_h_
 #define SDL_assert_c_h_
 
--- src/SDL_log.c
+++ src/SDL_log.c
@@ -1,23 +1,33 @@
-/*
-  Simple DirectMedia Layer
-  Copyright (C) 1997-2020 Sam Lantinga <slouken@libsdl.org>
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source distribution.
-*/
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*******************************************************************************
+ * Library       : SDLite 1.2.x
+ * Purpose       : Low-level access to a framebuffer, audio output and HID.
+ * Module        : Core
+ * Project       : Redux for Embedded System
+ * Description   : Stripped-down and optimized libraries for RISC processors
+ * License       : GNU General Public License v3.0
+ *******************************************************************************
+ *
+ * Rætro and SDLite 1.2.x:
+ * Copyright (c) 2019-2020 Marcus Andrade <marcus@raetro.org>
+ *
+ * Simple DirectMedia Layer and SDL:
+ * Copyright (c) 1997-2020 Sam Lantinga <slouken@libsdl.org>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.
+ * If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
+ *
+ ******************************************************************************/
 #include "SDL.h"
 
 /* Simple log messages in SDL */
@@ -234,114 +244,6 @@ void SDL_LogMessageV(int category, SDL_LogPriority priority, const char *fmt, va
 }
 
 static void SDLCALL SDL_LogOutput(void *userdata, int category, SDL_LogPriority priority, const char *message) {
-#if defined(__WIN32__) || defined(__WINRT__)
-	/* Way too many allocations here, urgh */
-	/* Note: One can't call SDL_SetError here, since that function itself logs. */
-	{
-		char *output;
-		size_t length;
-		LPTSTR tstr;
-		SDL_bool isstack;
-
-#if !defined(HAVE_STDIO_H) && !defined(__WINRT__)
-		BOOL attachResult;
-		DWORD attachError;
-		unsigned long charsWritten;
-		DWORD consoleMode;
-
-		/* Maybe attach console and get stderr handle */
-		if (consoleAttached == 0) {
-			attachResult = AttachConsole(ATTACH_PARENT_PROCESS);
-			if (!attachResult) {
-					attachError = GetLastError();
-					if (attachError == ERROR_INVALID_HANDLE) {
-						/* This is expected when running from Visual Studio */
-						/*OutputDebugString(TEXT("Parent process has no console\r\n"));*/
-						consoleAttached = -1;
-					} else if (attachError == ERROR_GEN_FAILURE) {
-						 OutputDebugString(TEXT("Could not attach to console of parent process\r\n"));
-						 consoleAttached = -1;
-					} else if (attachError == ERROR_ACCESS_DENIED) {
-						 /* Already attached */
-						consoleAttached = 1;
-					} else {
-						OutputDebugString(TEXT("Error attaching console\r\n"));
-						consoleAttached = -1;
-					}
-				} else {
-					/* Newly attached */
-					consoleAttached = 1;
-				}
-
-				if (consoleAttached == 1) {
-						stderrHandle = GetStdHandle(STD_ERROR_HANDLE);
-
-						if (GetConsoleMode(stderrHandle, &consoleMode) == 0) {
-							/* WriteConsole fails if the output is redirected to a file. Must use WriteFile instead. */
-							consoleAttached = 2;
-						}
-				}
-		}
-#endif /* !defined(HAVE_STDIO_H) && !defined(__WINRT__) */
-
-		length = SDL_strlen(SDL_priority_prefixes[priority]) + 2 + SDL_strlen(message) + 1 + 1 + 1;
-		output = SDL_small_alloc(char, length, &isstack);
-		SDL_snprintf(output, length, "%s: %s\r\n", SDL_priority_prefixes[priority], message);
-		tstr = WIN_UTF8ToString(output);
-
-		/* Output to debugger */
-		OutputDebugString(tstr);
-
-#if !defined(HAVE_STDIO_H) && !defined(__WINRT__)
-		/* Screen output to stderr, if console was attached. */
-		if (consoleAttached == 1) {
-				if (!WriteConsole(stderrHandle, tstr, lstrlen(tstr), &charsWritten, NULL)) {
-					OutputDebugString(TEXT("Error calling WriteConsole\r\n"));
-					if (GetLastError() == ERROR_NOT_ENOUGH_MEMORY) {
-						OutputDebugString(TEXT("Insufficient heap memory to write message\r\n"));
-					}
-				}
-
-		} else if (consoleAttached == 2) {
-			if (!WriteFile(stderrHandle, output, lstrlenA(output), &charsWritten, NULL)) {
-				OutputDebugString(TEXT("Error calling WriteFile\r\n"));
-			}
-		}
-#endif /* !defined(HAVE_STDIO_H) && !defined(__WINRT__) */
-
-		SDL_free(tstr);
-		SDL_small_free(output, isstack);
-	}
-#elif defined(__ANDROID__)
-	{
-		char tag[32];
-
-		SDL_snprintf(tag, SDL_arraysize(tag), "SDL/%s", GetCategoryPrefix(category));
-		__android_log_write(SDL_android_priority[priority], tag, message);
-	}
-#elif defined(__APPLE__) && (defined(SDL_VIDEO_DRIVER_COCOA) || defined(SDL_VIDEO_DRIVER_UIKIT))
-	/* Technically we don't need Cocoa/UIKit, but that's where this function is defined for now.
-	*/
-	extern void SDL_NSLog(const char *text);
-	{
-		char *text;
-		/* !!! FIXME: why not just "char text[SDL_MAX_LOG_MESSAGE];" ? */
-		text = SDL_stack_alloc(char, SDL_MAX_LOG_MESSAGE);
-		if (text) {
-			SDL_snprintf(text, SDL_MAX_LOG_MESSAGE, "%s: %s", SDL_priority_prefixes[priority], message);
-			SDL_NSLog(text);
-			SDL_stack_free(text);
-			return;
-		}
-	}
-#elif defined(__PSP__)
-	{
-		FILE*        pFile;
-		pFile = fopen ("SDL_Log.txt", "a");
-		fprintf(pFile, "%s: %s\n", SDL_priority_prefixes[priority], message);
-		fclose (pFile);
-	}
-#endif
 #if HAVE_STDIO_H
 	fprintf(stderr, "%s: %s\n", SDL_priority_prefixes[priority], message);
 #if __NACL__
@@ -363,5 +265,3 @@ void SDL_LogSetOutputFunction(SDL_LogOutputFunction callback, void *userdata) {
 	SDL_log_function = callback;
 	SDL_log_userdata = userdata;
 }
-
-/* vi: set ts=4 sw=4 expandtab: */
